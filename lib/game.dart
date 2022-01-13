fb1f119a73a8559a4027359c098ad19d04aace19
NanaChan <pathpeemeil@gmail.com>
2022-01-13 22:21:53 +0700

    work6

--- lib/game.dart
+++ lib/game.dart
@@ -3,6 +3,7 @@ class Game {
   //camel case
   int?  maxRandom;
   int? _answer;
+  int _guessCount = 0;
   static List<int> myList = [];
   Game({int maxRandom = 100}) {
     this.maxRandom = maxRandom;
@@ -10,10 +11,18 @@ class Game {
     _answer = r.nextInt(maxRandom) + 1;
   }
 
+  int get guessCount {
+    return _guessCount;
+  }
+  void addCountList() {
+    myList.add(_guessCount);
+  }
+
   int doGuess(int num) {
     // ทายถูก return 0
     // ทายมากไป return 1
     // ทายน้อยไป return -1
+    _guessCount++;
     if (num > _answer!) {
       return 1;
     } else if (num < _answer!) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:guess_number_week4/game.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,13 +19,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+  final _controller = TextEditingController();
+  var game = Game();
 
   @override
   Widget build(BuildContext context) {
@@ -32,24 +36,25 @@ class HomePage extends StatelessWidget {
         title: const Text('GUESS THE NUMBER'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(40.0),
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
         child: Container(
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20.0),
-              color: Colors.lightBlue.shade50,
-              //border: Border.all(width: 5.0, color: Colors.blue),
+              color: Colors.blue.shade50,
+              border: Border.all(width: 5.0, color: Colors.blueAccent),
               boxShadow: [
                 BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
+                    color: Colors.cyan.withOpacity(0.3),
                     offset: const Offset(2.0, 5.0),
                     blurRadius: 5.0,
                     spreadRadius: 2.0)
               ]),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
+          alignment: Alignment.center,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Expanded(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -59,21 +64,24 @@ class HomePage extends StatelessWidget {
                       height: 100,
                       //fit: BoxFit.fill,
                     ),
+
                   ),
                   Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+
                     children: const [
                       Text(
                         'GUESS',
                         style: TextStyle(
-                          color: Colors.cyan,
-                          fontSize: 50.0,
+                          color: Colors.blue,
+                          fontSize: 70.0,
                         ),
                       ),
                       Text(
                         'THE NUMBER',
                         style: TextStyle(
-                          color: Colors.deepPurple,
+                          color: Colors.blueAccent,
                           fontSize: 25.0,
                         ),
                       ),
@@ -81,19 +89,75 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 20.0,
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+
+                  textAlign: TextAlign.center,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.7),
+                    border: OutlineInputBorder(),
+                    hintText: 'ทายเลขตั้งแต่ 1 ถึง 100',
+                  ),
+                )),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: ElevatedButton(
+                child: Text('GUESS'),
+                onPressed: () {
+                  var input = _controller.text;
+                  var number = int.tryParse(input);
+                  String title = "RESULT";
+                  String content="";
+                  if(number == null) {
+                    title = "ERROR";
+                    content =
+                    "กรอกข้อมูลไม่ถูกต้อง ให้กรอกเฉพาะตัวเลขเท่านั้น";
+                  }
+                  else {
+                    var result = game.doGuess(number);
+                    if (result == 1) {
+                      content = "$number มากเกิน!! ลองใหม่" ;
+                    } else if (result == -1) {
+                      content = "$number น้อยเกิน!! ลองใหม่" ;
+                    } else if (result == 0) {
+                      content = " $number ถูกต้องละ \nทายไปทั้งหมด ${game.guessCount} ครั้ง" ;
+
+                    }
+
+                  }
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text(title),
+                        content: Text(content),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
               ),
-              const Padding(
-                padding:  EdgeInsets.all(32.0),
-                child:  TextField(),
-              ),
-              ElevatedButton(onPressed: (){}, child: const Text('GUESS')),
-            ],
-          ),
+            ),
+
+          ]),
         ),
       ),
     );
   }
 }
-
